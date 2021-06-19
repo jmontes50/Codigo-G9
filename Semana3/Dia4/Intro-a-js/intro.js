0927b379628d2a8a392b0b68e97ca59b3d08c44e
Josue Retamozo Vargas <josretamozovar@gmail.com>
2021-06-18 21:47:21 -0500

    intro a js

---
+++ Semana3/Dia4/Intro-a-js/intro.js
@@ -0,0 +1,42 @@
+console.log("Hola mundo");
+
+console.warn("Hey!!, soy una advertencia");
+
+console.error("");
+
+class Mesa {
+  // es como un plano para hacer mesas
+
+  imprimirNombre() {
+    console.log("Soy una mesa");
+  }
+}
+
+var taburete = new Mesa();
+
+taburete.imprimirNombre();
+
+//var saludo = "Hola , como estas?"
+
+let otroSaludo =
+  "Hola , como estas? , por favor evita usar la palabra reservada var cuando quieras usar una variable , usa let en su lugar";
+
+console.log(otroSaludo);
+
+otroSaludo = "Hola";
+
+console.log(otroSaludo);
+
+otroSaludo = 3;
+
+console.log(otroSaludo);
+
+const nombre = "Josue";
+
+console.log(nombre);
+
+nombre = "Faraon love shady 2";
+
+console.log(nombre);
+
+console.log("Hola")
