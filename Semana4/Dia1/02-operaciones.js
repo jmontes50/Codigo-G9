5db6166f7fd54b54d7a60291dc396db7403a7d13
Jorge Montesinos <osmarmontes50@gmail.com>
2021-06-21 19:41:35 -0500

    update data types y operaciones

---
+++ Semana4/Dia1/02-operaciones.js
@@ -0,0 +1,33 @@
+let a = 100;
+let b = 4;
+
+let suma = a + b;
+console.log("Suma", suma);
+
+let resta = b - a;
+console.log("Resta", resta);
+
+let multiplicacion = a * b;
+console.log("Multiplicar", multiplicacion);
+
+let division = a / b;
+console.log("División", division);
+
+let residuo = 105 % 4;
+console.log("Modulo", residuo);
+
+//Concatenar
+
+let nombre = "Osmar";
+
+let saludo = "Hola soy ";
+
+let edad = 30;
+
+// las variables que sean simples
+//sin tíldes, ni carácteres raros
+//comiencen en una letra
+let oracion = saludo + nombre + " y tengo " + edad;
+
+console.log(oracion)
+
