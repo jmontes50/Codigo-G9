64228f7fd3aebcb701e6dcc5dce974343c38e37d
Jorge Montesinos <osmarmontes50@gmail.com>
2021-06-21 19:14:04 -0500

    update js variables

---
+++ Semana4/Dia1/01-javascript.js
@@ -0,0 +1,54 @@
+console.log("Hola Código!");
+
+console.warn("Te advierto!");
+
+console.error('Algo ha pasado');
+
+//Variables
+//String - Texto
+var nombre = "Osmar";
+
+console.log(nombre);
+
+//Number - para operaciones aritméticas
+var edad = 30;
+
+console.log(typeof edad);
+
+//Boolean - Lógicas
+var esDeNoche = false;
+
+console.log(esDeNoche);
+
+var meQuiere = true;
+
+console.log(meQuiere);
+
+//tipos de datos raros
+//undefined
+var nota;
+
+console.log(nota);
+
+var nulo = null;
+
+console.log(nulo);
+
+var rareza = 1000/"patatas"
+
+console.log(rareza)
+//NaN, Not a Number
+//No es un número
+
+//let es mas seguro, var trae problemas
+let edadPerro = 15;
+//si yo deseo cambiar el valor de una variable, me basta con referenciarla y darle un nuevo valor con =
+edadPerro = 17;
+
+edadPerro = 20;
+
+edadPerro = "veinte"
+
+edadPerro = undefined
+
+console.log(edadPerro);
