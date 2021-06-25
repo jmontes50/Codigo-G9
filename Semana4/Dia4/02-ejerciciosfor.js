50cd8bec2c56dbd388d7600c494b1da42fc9393c
Jorge Montesinos <osmarmontes50@gmail.com>
2021-06-24 21:12:34 -0500

    update

--- Semana4/Dia4/02-ejerciciosfor.js
+++ Semana4/Dia4/02-ejerciciosfor.js
@@ -47,12 +47,56 @@ let notas = [15,17,20,20,14,18,14]
 
 //Si utilicen el ejemplo hicimos antes
 
-let anioInicio = +prompt("Indique un año inicial")
+// let anioInicio = +prompt("Indique un año inicial")
 
-let anioFinal = +prompt("Indique un año final")
+// let anioFinal = +prompt("Indique un año final")
 
-for(let i = anioInicio; i <= anioFinal; i++){
-  if((i % 4 == 0 && i %100 != 0) || i % 400 == 0){
-    console.log(`El año ${i} es bisiesto!`)
+// for(let i = anioInicio; i <= anioFinal; i++){
+//   if((i % 4 == 0 && i %100 != 0) || i % 400 == 0){
+//     console.log(`El año ${i} es bisiesto!`)
+//   }
+// }
+
+//Ejercicio 5
+/**
+ * Una bodega quiere hacer un inventario, actualmente.tiene 03 tipos de productos: papitas, jabones y chocolates
+ * queremos saber cuantos productos tiene en existencia
+ */
+/**t
+ * Descomposición
+ * Reglas: Manejamos 3 productos
+ * Cada Producto tendrá su cantidad al final
+ * Tendremos que preguntar producto por producto
+ * -Que datos o variables deberiamos tener?
+ * 1. variables para contar c/producto
+ * 2. cantidad total de productos en general
+ */
+
+//Ud pregunten cada vez el tipo de producto que esten contando en ese momento
+
+let cantProductos = +prompt("Cuantos productos contarás?");
+
+let papitas = 0;
+let chocolates = 0;
+let jabones = 0;
+
+for(let i = 1; i <= cantProductos; i++){
+  let tipo = +prompt('Ingrese 1. Papitas, 2. Choco 3. Jabones');
+
+  switch(tipo){
+    case 1:
+      papitas++;
+      break;
+    case 2:
+      chocolates++;
+      break;
+    case 3:
+      jabones++;
+      break;
+    default:
+      alert('Ingresa bien >:o')
+      i-- //reduzco el valor que tiene i para que no lo considere, porque esta mal
   }
 }
+
+console.log(`En total tenemos ${papitas} papitas, ${chocolates} chocolates, ${jabones} jabones`)
