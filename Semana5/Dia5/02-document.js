6d695fdaa3297a3970bb171c82dbe5c33351ff71
Jorge Montesinos <osmarmontes50@gmail.com>
2021-07-02 21:01:34 -0500

    update

--- Semana5/Dia5/02-document.js
+++ Semana5/Dia5/02-document.js
@@ -43,3 +43,37 @@ cajitas.forEach((caja) => {
 
   console.log(caja.classList)
 })
+
+let miParrafo = document.querySelector("#miParrafo")
+
+console.log(miParrafo)
+
+miParrafo.classList.add("resaltado")
+
+// setTimeout(()=>{
+//   miParrafo.classList.remove("resaltado")
+// }, 4000)
+
+//setInterval se ejecuta CADA X tiempo
+setInterval(() => {
+  //toggle si encuentra algo lo quita y si no lo encuentra lo agrega
+  miParrafo.classList.toggle("resaltado")
+}, 2000)
+
+//setInterval(una funcion, tiempo)
+//cada x tiempo ejecutará la función
+// setInterval(() => {
+//   console.log("segundo!")
+// }, 1000)
+
+let divContenido = document.getElementById("contenido")
+
+divContenido.innerHTML = "<p>párrafo desde JS</p>"
+
+//document.createElement("etiqueta")
+//crea un objeto element (elementoHTML) de forma nativa
+let parrafo2 = document.createElement("p")
+parrafo2.innerHTML = "Texto del párrafo 2"
+
+//appendChild hace que un elemento se añada como hijo de otro Elemento 
+divContenido.appendChild(parrafo2)
