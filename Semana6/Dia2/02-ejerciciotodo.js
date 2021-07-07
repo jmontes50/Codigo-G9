4a2e2c0aabeec0314443d3139c94b1affc62fdf8
Jorge Montesinos <osmarmontes50@gmail.com>
2021-07-06 22:04:04 -0500

    updsate

--- Semana6/Dia2/02-ejerciciotodo.js
+++ Semana6/Dia2/02-ejerciciotodo.js
@@ -10,7 +10,11 @@ let formTarea = document.getElementById("formTarea")
 
 let ulPendientes = document.getElementById("pendientes")
 
+let ulCompletados = document.getElementById("completados")
+
 let arregloPendientes = []
+
+let arregloCompletado = []
 
 formTarea.addEventListener("submit", (evento) => {
   evento.preventDefault()
@@ -37,16 +41,36 @@ let dibujarPendientes = () => {
     li.innerHTML = item
     ulPendientes.appendChild(li)
 
+    //Manera - 1
     li.addEventListener("dblclick", () => {
       //remuevo unos elementos del arreglo
-      arregloPendientes.splice(i, 1)
-      //remuevo del navegador, el mismo elemento
-      ulPendientes.removeChild(ulPendientes.childNodes[i])
-      console.log(arregloPendientes)
+      arregloCompletado.push(arregloPendientes[i])
+      arregloPendientes.splice(i, 1) //elimino un elemento
+      dibujarPendientes() //vuelvo a dibujar
+      dibujarCompletados()
+      console.log(arregloCompletado)
     })
+
+    // Manera - 2 - bug al quedarse 1 solo elemento
+    // li.addEventListener("dblclick", function() {
+    //   //remuevo unos elementos del arreglo
+    //   console.log(i)
+    //   arregloPendientes.splice(i, 1)
+    //   //remuevo del navegador, el mismo elemento
+    //   ulPendientes.removeChild(this)
+    //   console.log(arregloPendientes)
+    // })
   })
   // let li = document.createElement("li")
   // li.innerHTML = arregloPendientes[arregloPendientes.length - 1]//ultimo elemento
   // ulPendientes.appendChild(li)
+}
 
+let dibujarCompletados = () => {
+  ulCompletados.innerHTML = ""
+  arregloCompletado.forEach((item) => {
+    let li = document.createElement("li")
+    li.innerHTML = item
+    ulCompletados.appendChild(li)
+  })
 }
