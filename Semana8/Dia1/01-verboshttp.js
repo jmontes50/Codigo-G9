c893a75a8c1df2faaa963b37849233c47d294180
Jorge Montesinos <osmarmontes50@gmail.com>
2021-07-19 20:30:25 -0500

    update

--- Semana8/Dia1/01-verboshttp.js
+++ Semana8/Dia1/01-verboshttp.js
@@ -15,5 +15,37 @@ const obtenerEstudiantes = () => {
     })
 }
 
-obtenerEstudiantes()
+// obtenerEstudiantes()
 
+const crearEstudiante = () => {
+    const nuevoEstudiante = {
+        nombres:"Tony",
+        apellidos:"Stark",
+        telefono:70979038
+    }
+    const cabecera = {
+        method:'POST',//PUT,DELETE
+        headers:{
+            //para que la API sepa como procesarlo
+            'Content-Type':'application/json'
+        },
+        //lo convertimos a un JSON
+        body:JSON.stringify(nuevoEstudiante)
+    }
+
+    fetch("https://60f60ddc18254c00176e0100.mockapi.io/estudiantes", cabecera)
+    .then((respuesta) => {
+        //info del response
+        console.log(respuesta)
+        return respuesta.json() //la respuesta la convierte a JS
+    })
+    .then((estudianteCreado) => {
+        console.log(estudianteCreado)
+    })
+    .catch((err) => {
+        console.error(err)
+    })
+
+}
+
+crearEstudiante()
