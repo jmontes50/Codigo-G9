28b40108b06b8b96cd389a6ed890a9302a04b6cf
Jorge Montesinos <osmarmontes50@gmail.com>
2021-07-19 21:01:22 -0500

    update

--- Semana8/Dia1/01-verboshttp.js
+++ Semana8/Dia1/01-verboshttp.js
@@ -19,9 +19,10 @@ const obtenerEstudiantes = () => {
 
 const crearEstudiante = () => {
     const nuevoEstudiante = {
-        nombres:"Tony",
-        apellidos:"Stark",
-        telefono:70979038
+        nombres:"Natasha",
+        apellidos:"Rommanoff",
+        telefono:70979038,
+        direccion:"S/N"
     }
     const cabecera = {
         method:'POST',//PUT,DELETE
@@ -33,6 +34,7 @@ const crearEstudiante = () => {
         body:JSON.stringify(nuevoEstudiante)
     }
 
+    //fetch(url, configuracion)
     fetch("https://60f60ddc18254c00176e0100.mockapi.io/estudiantes", cabecera)
     .then((respuesta) => {
         //info del response
@@ -48,4 +50,36 @@ const crearEstudiante = () => {
 
 }
 
-crearEstudiante()
+// crearEstudiante()
+
+const actualizarEstudiante = (id) => {
+    //pongo los campos que tenga en mis api, solo los que quiero modificar/actualizar
+    const estudianteModificado = {
+        nombres:"Antonio"
+    }
+    //indico que el método será PUT
+    const configuracion = {
+        method:'PUT',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(estudianteModificado)
+    }
+
+    fetch(`https://60f60ddc18254c00176e0100.mockapi.io/estudiantes/${id}`, configuracion)
+    .then((respuesta) => {
+        console.log(respuesta)
+        return respuesta.json()
+    })
+    .then((estudianteActualizado) => {
+        console.log(estudianteActualizado)
+    })
+    .catch((err) => {
+        console.error(err)
+    })
+    // .finally(() => {
+
+    // })
+}
+
+actualizarEstudiante(6)
