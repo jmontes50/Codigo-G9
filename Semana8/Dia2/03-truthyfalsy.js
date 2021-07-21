22c0982f54fc095124ce5c4f0543262b9393f7e0
Jorge Montesinos <osmarmontes50@gmail.com>
2021-07-20 21:03:30 -0500

    update

---
+++ Semana8/Dia2/03-truthyfalsy.js
@@ -0,0 +1,23 @@
+let valor = 8;
+
+if(valor){
+    console.log("esta definido")
+}
+
+let valor2 = null;
+
+if(valor2){
+    console.log("va bien")
+}else{
+    console.log("algo no va bien")
+}
+
+let texto = "";
+
+if(texto){
+    console.log("texto va bien")
+}else{
+    console.log("texto no va bien")
+}
+
+texto ? console.log("tiene letras") : console.log("no tiene naaaaa")
