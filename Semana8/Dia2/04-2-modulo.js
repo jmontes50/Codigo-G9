9faaee17de54fc18e69085d75eaf50436b45eb78
Jorge Montesinos <osmarmontes50@gmail.com>
2021-07-20 22:34:49 -0500

    updaTE

---
+++ Semana8/Dia2/04-2-modulo.js
@@ -0,0 +1,13 @@
+let saludar = (texto) => {
+    return `Hola, ${texto}`
+}
+
+let url = "http://url"
+
+let token = "3gsajifbsdhuifbsd78123"
+
+export {
+    saludar,
+    url,
+    token as default //solamente 1
+}
---
+++ Semana8/Dia2/04-modulo.js
@@ -0,0 +1,13 @@
+import patata, {saludar, url} from './04-2-modulo.js'
+//import que queramos
+// import {saludar} from './04-2-modulo'
+// import * as ALIAS from "ruta"
+// import * as miModulo from "./04-2-modulo.js"
+
+console.log(saludar("amig@s peruan@s"))
+
+console.log(url)
+
+console.log(patata)
+
+// console.log(miModulo.default)
