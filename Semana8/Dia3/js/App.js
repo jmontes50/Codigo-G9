dfeeff3f2384a370fb0736df09c305994d01ca2c
Jorge Montesinos <osmarmontes50@gmail.com>
2021-07-21 21:18:47 -0500

    update

--- Semana8/Dia3/js/App.js
+++ Semana8/Dia3/js/App.js
@@ -4,9 +4,17 @@
 //2. Convertir esos datos a HTML
 //3. Agregarlos al DOM
 
+//primero imports
 import { obtenerProductos } from "./productoService.js"
 import { imprimirProductos } from "./Interfaz.js"
 
+//variables globales
+const btnCrear = document.getElementById("btnCrear")
+const modalCrear = document.getElementById("modalCrear")
+const bsModalCrear = new bootstrap.Modal(modalCrear)
+const formCrear = document.getElementById("formCrear")
+
+//codigo
 const getProductos = () => {
     obtenerProductos()
     .then(productos => {
@@ -15,3 +23,22 @@ const getProductos = () => {
     })
 }
 getProductos()
+
+btnCrear.addEventListener("click", () => {
+    bsModalCrear.show()
+})
+
+//escuchar el submit
+formCrear.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const nuevoProducto = {
+        prod_nombre: formCrear.prod_nombre.value,
+        prod_descripcion: formCrear["prod_descripcion"].value,
+        prod_precio: formCrear["prod_precio"].value,
+        prod_stock: formCrear["prod_stock"].value,
+        prod_oferta: formCrear["prod_oferta"].value,
+    }
+
+    console.log(nuevoProducto)
+})
