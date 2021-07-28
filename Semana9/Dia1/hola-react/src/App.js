3d47ac0d821de5db1d62d265195fe4552f5eb5ba
Jorge Montesinos <osmarmontes50@gmail.com>
2021-07-27 22:03:49 -0500

    update

--- Semana9/Dia1/hola-react/src/App.js
+++ Semana9/Dia1/hola-react/src/App.js
@@ -5,9 +5,14 @@ import ListaTareas from './components/ListaTareas'
 export default function App() {
   //const [estado, funcDelEstado] = useState(estadoInicial)
   const [tareas, setTareas] = useState(["pasear al perro"])
+  const [texto, setTexto] = useState("ordenar")
 
   const anadirTarea = () => {
-    setTareas([...tareas, "lavar la ropa"])
+    setTareas([...tareas, texto])
+  }
+
+  const manejarTexto = (nuevoTexto) => {
+    setTexto(nuevoTexto)
   }
 
   const miTitulo = "Mi App"
@@ -17,7 +22,9 @@ export default function App() {
       <h1>{miTitulo}</h1>
       <ListaTareas tareas={tareas}/>
       <hr/>
-      
+
+      <input type="text" value={texto} onChange={(e) => {manejarTexto(e.target.value)}}/>
+
       <button onClick={anadirTarea}>
         Anadir Tarea
       </button>
