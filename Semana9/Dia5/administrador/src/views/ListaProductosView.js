1f781e629f832c6c4d6f07d9f051bd11f187fa81
Jorge Montesinos <osmarmontes50@gmail.com>
2021-08-02 19:37:29 -0500

    update

--- Semana9/Dia5/administrador/src/views/ListaProductosView.js
+++ Semana9/Dia5/administrador/src/views/ListaProductosView.js
@@ -7,7 +7,8 @@ export default function ListaProductosView() {
     const getProductos = async () => {
         try {
             const productosObtenidos = await obtenerProductos()
-            console.log(productosObtenidos)
+            // console.log(productosObtenidos)
+            setProductos(productosObtenidos)
         } catch (error) {
             console.log(error)
         }
@@ -20,7 +21,28 @@ export default function ListaProductosView() {
 
     return (
         <div>
-            
+            <h1>Productos registrados</h1>
+
+            <table className="table">
+                <thead>
+                    <th>Nombre</th>
+                    <th>Precio</th>
+                    <th>Descripción</th>
+                    <th>Stock</th>
+                    <th>Acciones</th>
+                </thead>
+                <tbody>
+                    {productos.map((prod, i) => (
+                        <tr key={i}>
+                            <td>{prod.prod_nombre}</td>
+                            <td>{prod.prod_precio}</td>
+                            <td>{prod.prod_descripcion}</td>
+                            <td>{prod.prod_stock}</td>
+                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
