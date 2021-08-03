0fe71bd78f924587b27fd7d1d6b338e5da97212f
Jorge Montesinos <osmarmontes50@gmail.com>
2021-08-02 21:04:53 -0500

    update

--- Semana9/Dia5/administrador/src/views/ListaProductosView.js
+++ Semana9/Dia5/administrador/src/views/ListaProductosView.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react'
+import {Link} from "react-router-dom"
 import { obtenerProductos } from '../services/productosService'
 
 export default function ListaProductosView() {
@@ -22,9 +23,9 @@ export default function ListaProductosView() {
     return (
         <div>
             <h1>Productos registrados</h1>
-            <button className="btn btn-primary btn-lg my-2">
+            <Link className="btn btn-primary btn-lg my-2" to="/crear">
                 Crear Producto
-            </button>
+            </Link>
             <table className="table">
                 <thead>
                     <tr>
