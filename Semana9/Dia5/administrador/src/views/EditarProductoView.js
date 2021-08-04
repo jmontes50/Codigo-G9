47492d53eeadcbbf517f6c5e4878123d49d629da
Jorge Montesinos <osmarmontes50@gmail.com>
2021-08-03 20:53:31 -0500

    update

--- Semana9/Dia5/administrador/src/views/EditarProductoView.js
+++ Semana9/Dia5/administrador/src/views/EditarProductoView.js
@@ -1,15 +1,23 @@
 import { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom"
 import { editarProducto, obtenerProductoPorId } from '../services/productosService'
+import FormProducto from '../components/FormProducto'
 
 export default function EditarProductoView() {
+    const [value, setValue] = useState({
+        prod_nombre:'',
+        prod_descripcion:'',
+        prod_precio:0,
+        prod_stock:0,
+        prod_oferta:false,
+    })
     //useParams en forma de objeto me va a dar los parámetros que este recibiendo por la URL
     const {id} = useParams()
 
     const getProducto = async () => {
         try {
             const productoObtenido = await obtenerProductoPorId(id)
-            console.log(productoObtenido)
+            setValue({...productoObtenido})
         } catch (error) {
             console.error(error)
         }
@@ -19,9 +27,32 @@ export default function EditarProductoView() {
         getProducto()
     },[])
 
+    const actualizarInput = (e) => {
+        if(e.target.name === "prod_oferta"){
+            setValue({
+                ...value,
+                [e.target.name]:e.target.checked
+            })
+            return
+        }
+        setValue({
+            ...value,
+            [e.target.name]:e.target.value
+        })
+    }
+
+    const manejarSubmit = () => {
+
+    }
+
     return (
         <div>
             <h1>Editar Producto</h1>
+            <FormProducto 
+                value={value} 
+                actualizarInput={actualizarInput}
+                manejarSubmit={}
+            />
         </div>
     )
 }
