ff228fa8ec3fabfa35844aab33a6ff81c76037ef
Jorge Montesinos <osmarmontes50@gmail.com>
2021-08-03 20:31:22 -0500

    update

--- Semana9/Dia5/administrador/src/views/EditarProductoView.js
+++ Semana9/Dia5/administrador/src/views/EditarProductoView.js
@@ -1,9 +1,23 @@
 import { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom"
+import { editarProducto, obtenerProductoPorId } from '../services/productosService'
 
 export default function EditarProductoView() {
     //useParams en forma de objeto me va a dar los parámetros que este recibiendo por la URL
     const {id} = useParams()
+
+    const getProducto = async () => {
+        try {
+            const productoObtenido = await obtenerProductoPorId(id)
+            console.log(productoObtenido)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        getProducto()
+    },[])
 
     return (
         <div>
