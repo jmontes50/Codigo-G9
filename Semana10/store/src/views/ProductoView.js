1b2e5dec619ca9fe05b37592fb638cf7db3743e3
Jorge Montesinos <osmarmontes50@gmail.com>
2021-08-05 20:58:16 -0500

    update

--- Semana10/store/src/views/ProductoView.js
+++ Semana10/store/src/views/ProductoView.js
@@ -1,9 +1,30 @@
 import {useState, useEffect} from 'react'
+import {useParams} from "react-router-dom"
+import { obtenerProductoPorId } from '../services/productosService'
+import Loading from '../components/Loading'
 
 export default function ProductoView() {
+    const [producto, setProducto] = useState({})
+    const [cargando, setCargando] = useState(true)
+
+    const { id } = useParams()
+
+    const getProducto = async () => {
+        try {
+            let productoObtenido = await obtenerProductoPorId(id)
+            setProducto(productoObtenido)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        getProducto()
+    }, [])
+
     return (
         <div>
-            
+           {cargando ? (<Loading />) : null}
         </div>
     )
 }
