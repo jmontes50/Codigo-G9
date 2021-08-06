fb8337a779dafe624ae91c9743a8d24e088bd647
Jorge Montesinos <osmarmontes50@gmail.com>
2021-08-06 18:44:53 -0500

    update

---
+++ Semana10/store/src/context/carritoContext.js
@@ -0,0 +1,20 @@
+import { useState, createContext } from "react";
+
+export const CarritoContext = createContext()
+
+const CarritoContextProvider = (props) => {
+
+    const [carrito, setCarrito] = useState([])
+
+    const anadirACarrito = (producto) => {
+        setCarrito([...carrito, producto])
+    }
+
+    return (
+        <CarritoContext.Provider value={{carrito, anadirACarrito}}>
+            {props.children}
+        </CarritoContext.Provider>
+    )
+}
+
+export default CarritoContextProvider
