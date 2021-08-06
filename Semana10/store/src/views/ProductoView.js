e8a1913966336169cc0426f8f1c7bb30e5a7e39c
Jorge Montesinos <osmarmontes50@gmail.com>
2021-08-05 21:12:59 -0500

    update

--- Semana10/store/src/views/ProductoView.js
+++ Semana10/store/src/views/ProductoView.js
@@ -13,6 +13,7 @@ export default function ProductoView() {
         try {
             let productoObtenido = await obtenerProductoPorId(id)
             setProducto(productoObtenido)
+            setCargando(false)
         } catch (error) {
             console.error(error)
         }
@@ -24,7 +25,31 @@ export default function ProductoView() {
 
     return (
         <div>
-           {cargando ? (<Loading />) : null}
+           {cargando ? 
+           (<Loading />) : 
+           (<div>
+               <div className="container">
+                   <h2 className="fw-bold">{producto.prod_nombre}</h2>
+                   <div className="row">
+                        <div className="col-sm-12 col-md-6">
+                            <img 
+                                className="img-fluid"
+                                src={producto.prod_imagen}
+                                alt={producto.prod_nombre}
+                            />
+                        </div>
+                        <div className="col-sm-12 col-md-6">
+                            <h5 className="fw-bold">Descripción</h5>
+                            <p>{producto.prod_descripcion}</p>
+                            <div className="py-3 d-flex justify-content-between">
+                                <span className="fw-bold">
+                                    S/ {producto.prod_precio}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+           </div>)}
         </div>
     )
 }
