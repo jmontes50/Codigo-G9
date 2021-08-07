eb556f781d4d5cc3a08c0ffdb509eaf80a89519b
Jorge Montesinos <osmarmontes50@gmail.com>
2021-08-06 20:12:37 -0500

    update

--- Semana10/store/src/context/carritoContext.js
+++ Semana10/store/src/context/carritoContext.js
@@ -7,7 +7,21 @@ const CarritoContextProvider = (props) => {
     const [carrito, setCarrito] = useState([])
 
     const anadirACarrito = (producto) => {
-        setCarrito([...carrito, producto])
+        for(let i = 0; i < carrito.length; i++){
+            if(carrito[i].id === producto.id){
+                //significa que tenemos el producto ya dentro del carrito
+                const productoExiste = {
+                    ...carrito[i],
+                    cantidad: carrito[i].cantidad + 1
+                }
+                let carritoTmp = [...carrito] //como carrito es un estado, es inmutable, x eso tenemos una copia
+                carritoTmp.splice(i, 1) //remuevo el producto que aumentará su cantidad
+                carritoTmp.push(productoExiste) //vuelvo a agregar el producto pero con su cantidad actualizada
+                setCarrito(carritoTmp) //actualizo el carrito con la copia actualizada
+                return //corto la ejecución aquí
+            }
+        }
+        setCarrito([...carrito, {...producto, cantidad:1}])
     }
 
     return (
