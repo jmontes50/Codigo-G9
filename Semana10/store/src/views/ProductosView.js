2ba3917ec89e0273732913186372117fae9eef52
Jorge Montesinos <osmarmontes50@gmail.com>
2021-08-07 10:18:33 -0500

    update

--- Semana10/store/src/views/ProductosView.js
+++ Semana10/store/src/views/ProductosView.js
@@ -2,10 +2,12 @@ import { useState, useEffect } from 'react'
 import { obtenerProductos } from '../services/productosService'
 import Loading from '../components/Loading'
 import GroupProducts from '../components/GroupProducts'
+import Slider from '@material-ui/core/Slider';
 
 export default function ProductosView() {
     const [productos, setProductos] = useState([])
     const [cargando, setCargando] = useState(true)
+    const [filtroPrecio, setFiltroPrecio] = useState([1, 100])
 
     const getProductos = async () => {
         try {
@@ -15,6 +17,10 @@ export default function ProductosView() {
         } catch (error) {
             console.error(error)
         }
+    }
+
+    const manejarPrecio = (evento, nuevosPrecios) => {
+        setFiltroPrecio(nuevosPrecios)
     }
 
     useEffect(() => {
@@ -31,6 +37,14 @@ export default function ProductosView() {
                         <i className="fas fa-gifts me-3" />
                         Nuestros Productos
                     </h1>
+                    <div className="row my-2">
+                       <div className="col-sm-12 col-md-6">
+                           <h5>Filtrar Por precio</h5>
+                           <Slider
+                                value={filtroPrecio}
+                            />
+                       </div>
+                    </div>
                 </div>
                 <GroupProducts productos={productos} />
             </div>)}
