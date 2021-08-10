b8c66f6ee4b9b4aad60a56eb0f4082d574e6656d
Jorge Montesinos <osmarmontes50@gmail.com>
2021-08-10 18:46:29 -0500

    update

--- Semana10/store/src/components/NavTop.js
+++ Semana10/store/src/components/NavTop.js
@@ -22,39 +22,40 @@ export default function NavTop() {
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
-						<Link to="/">Inicio</Link>
+						<Link className="nav-link" to="/">Inicio</Link>
 
-						<Link to="/productos">Productos</Link>
+						<Link className="nav-link" to="/productos">Productos</Link>
 					</Nav>
+					<Nav>
+						<Link className="nav-link" to="/carrito">
+							Carrito
+							<Badge badgeContent={totalCarrito} color="primary">
+								<ShoppingCartIcon />
+							</Badge>
+						</Link>
 
-					<Link to="/carrito">
-						Carrito
-						<Badge badgeContent={totalCarrito} color="primary">
-							<ShoppingCartIcon />
-						</Badge>
-					</Link>
+						<Link className="nav-link" to="/checkout">Comprar!</Link>
 
-					<Link to="/checkout">Comprar!</Link>
-
-					{userState ? (
-						<NavDropdown
-							title={
-								<div className="d-inline">
-									<img
-										src={userState.photoURL}
-										className="me-2"
-										style={{ borderRadius: "50%", width: "30px" }}
-										alt="avatar"
-									/>
-									<span>{userState.displayName}</span>
-								</div>
-							}
-						>
-							<NavDropdown.Item onClick={signOut}>Salir</NavDropdown.Item>
-						</NavDropdown>
-					) : (
-						<Link to="/login">Ingresar</Link>
-					)}
+						{userState ? (
+							<NavDropdown
+								title={
+									<div className="d-inline">
+										<img
+											src={userState.photoURL}
+											className="me-2"
+											style={{ borderRadius: "50%", width: "30px" }}
+											alt="avatar"
+										/>
+										<span>{userState.displayName}</span>
+									</div>
+								}
+							>
+								<NavDropdown.Item onClick={signOut}>Salir</NavDropdown.Item>
+							</NavDropdown>
+						) : (
+							<Link className="nav-link" to="/login">Ingresar</Link>
+						)}
+					</Nav>
 				</Navbar.Collapse>
 			</Container>
 		</Navbar>
